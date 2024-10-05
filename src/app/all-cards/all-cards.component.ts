c02dabd257aa4362610d13884a74ecaf8c443d6f
Bruno1224 <brunomenna2011@gmail.com>
2024-10-05 17:32:22 -0300

    all cards

--- src/app/all-cards/all-cards.component.ts
+++ src/app/all-cards/all-cards.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit } from '@angular/core';
+import { AllCardsService } from './all-cards.service'
+import { CommonModule } from '@angular/common';
+interface Card {
+  id: number;
+  name: string;
+  card_images: Array<{ image_url: string }>;
+}
 @Component({
   selector: 'app-all-cards',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './all-cards.component.html',
   styleUrl: './all-cards.component.css'
 })
-export class AllCardsComponent {
 
+export class AllCardsComponent implements OnInit {
+  cards: Card[] = [];
+
+  constructor(private allCardsService: AllCardsService) { }
+
+  ngOnInit(): void {
+    this.fetchCards();
+  }
+
+  async fetchCards() {
+    try {
+      this.cards = await this.allCardsService.getAllCards();
+    } catch (error) {
+      console.error('Error fetching cards:', error);
+    }
+  }
 }
