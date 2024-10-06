f09cd14a5787bf09fe765e3cb966f4655d70b5f3
Agusg10 <agusgioia@gmail.com>
2024-10-05 23:05:39 -0300

    actualizar

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,33 +1,20 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [],
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: 'app-home', // Cambia esto por el nombre de tu componente
+  templateUrl: './home.component.html', // Cambia esto por tu archivo HTML
+  styleUrls: ['./home.component.css'], // Cambia esto por tu archivo CSS
+  standalone:true,
+  imports:[]
 })
 export class HomeComponent {
-  @ViewChild('audioPlayer') audioPlayer!: ElementRef; // Asegúrate de que esté correctamente inicializado
-
-  ngAfterViewInit(){
-    if (this.audioPlayer) {
-      console.log('El elemento de audio ha sido inicializado:', this.audioPlayer.nativeElement);
-    } else {
-      console.error('El elemento de audio no está disponible');
-    }
+  
+  playAudio(audio: HTMLAudioElement) {
+    audio.play().catch(error => console.error('Error al reproducir el audio:', error));
   }
 
-  playAudio() {
-    if (this.audioPlayer) {
-      this.audioPlayer.nativeElement.play();
-    }
-  }
-
-  stopAudio() {
-    if (this.audioPlayer) {
-      this.audioPlayer.nativeElement.pause();
-      this.audioPlayer.nativeElement.currentTime = 0; // Reinicia el audio al inicio
-    }
+  stopAudio(audio: HTMLAudioElement) {
+    audio.pause();
+    audio.currentTime = 0; // Reinicia el audio
   }
 }
