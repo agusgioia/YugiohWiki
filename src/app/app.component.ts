e2769c0eb56a0d8188ab6fa1a1951c02f7750267
Agusg10 <agusgioia@gmail.com>
2024-10-09 19:03:37 -0300

    Buscador hecho

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,12 @@ import { RouterOutlet, RouterLink } from '@angular/router';
 import { FooterComponent } from "./footer/footer.component";
 import { HeaderComponent } from "./header/header.component";
 import { CommonModule } from '@angular/common';
-
+import { SearchComponent } from './search/search.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [RouterOutlet, RouterLink, FooterComponent, HeaderComponent, CommonModule],
+  imports: [RouterOutlet, RouterLink, FooterComponent, HeaderComponent, CommonModule,SearchComponent],
   standalone:true
 })
 export class AppComponent {
